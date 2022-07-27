dbacfbed276c00deda9e09d3cf080853145180f4
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2022-07-27 03:43:36 -0300

    cambios

---
+++ confirmacion-envio.php
@@ -0,0 +1,37 @@
+<?php
+$pg = "contacto";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+    <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+    <script src="css/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="shortcut icon" href="./images/Imagenes/favicon.ico" type="image/x-icon">
+</head>
+
+<body>
+
+    <head>
+        <?php include_once("menu.php"); ?>
+    </head>
+
+    <body>
+
+    </body>
+
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
